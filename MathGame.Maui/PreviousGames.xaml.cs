a477f4e5340c8b22a23c6954a8973b8acdd7c5c4
bigvvill <wcurtisusa@gmail.com>
2022-09-29 17:11:38 -0400

    Add project files.

---
+++ MathGame.Maui/PreviousGames.xaml.cs
@@ -0,0 +1,20 @@
+namespace MathGame.Maui;
+
+public partial class PreviousGames : ContentPage
+{
+	public PreviousGames()
+	{
+		InitializeComponent();
+		App.GameRepository.GetAllGames();
+		gamesList.ItemsSource = App.GameRepository.GetAllGames();
+	}
+
+	private void OnDelete(object sender, EventArgs e)
+	{
+		ImageButton button = (ImageButton)sender;
+
+		App.GameRepository.Delete((int)button.BindingContext);
+
+		gamesList.ItemsSource = App.GameRepository.GetAllGames();
+	}
+}
